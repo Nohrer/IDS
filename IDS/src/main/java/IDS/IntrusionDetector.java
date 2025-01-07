a7eb4797e7beaa9c1b4d25a9fa2c82274225d16c
Soufiane <Soufianekousta1@gmail.com>
2025-01-07 20:29:49 +0100

    version chi7aja 3

--- IDS/src/main/java/IDS/IntrusionDetector.java
+++ IDS/src/main/java/IDS/IntrusionDetector.java
@@ -1,36 +1,23 @@
 package IDS;
 
-import org.pcap4j.packet.IpV4Packet;
-import org.pcap4j.packet.IpV6Packet;
 import org.pcap4j.packet.Packet;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IntrusionDetector {
+    private final List<Rule> rules = new ArrayList<>();
 
-    private final Set<String> blacklistedIPs = new HashSet<>();
-
-    // Add an IP to the blacklist
-    public void addBlacklistedIP(String ip) {
-        blacklistedIPs.add(ip);
+    public void addRule(Rule rule) {
+        rules.add(rule);
     }
 
-    // Detect intrusion based on blacklisted IPs
     public void detectIntrusion(Packet packet) {
-        String srcIp = extractSourceIP(packet);
-        if (srcIp != null && blacklistedIPs.contains(srcIp)) {
-            System.out.println("ALERT: Intrusion detected from IP " + srcIp);
-            //more actions
-            }
-    }
+        for (Rule rule : rules) {
+            if (rule.evaluate(packet)) {
+                System.out.println("ALERT: Rule violation detected! " + rule.getDescription());
 
-    private String extractSourceIP(Packet packet) {
-        if (packet.contains(IpV4Packet.class)) {
-            return packet.get(IpV4Packet.class).getHeader().getSrcAddr().getHostAddress();
-        } else if (packet.contains(IpV6Packet.class)) {
-            return packet.get(IpV6Packet.class).getHeader().getSrcAddr().getHostAddress();
+            }
         }
-        return null; // Return null for non-IP packets
     }
 }
