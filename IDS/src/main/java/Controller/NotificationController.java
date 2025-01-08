67a9a2af23105b57af67ef36f0d7f7086ef03ebb
nohrer <naoufelguendouz03@gmail.com>
2025-01-08 01:31:45 +0100

    a

--- IDS/src/main/java/Controller/NotificationController.java
+++ IDS/src/main/java/Controller/NotificationController.java
@@ -1,7 +1,6 @@
 package Controller;
 
 import App.IdsApplication;
-import IDS.Notification;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
